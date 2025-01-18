d284888a74319d63d5f6920b7e495bbc42e2683e
Luiz Fernando <luizfvidal3@hotmail.com>
2025-01-18 06:38:17 -0300

    first commit

---
+++ index.dart
@@ -0,0 +1,11 @@
+void main() {
+  //? Definindo variáveis
+  int numero1 = 25555;
+  int numero2 = 205;
+
+  //? Realizando a soma
+  int soma = numero1 + numero2;
+
+  //? Imprimindo o resultado
+  print('A soma de $numero1 e $numero2 é: $soma');
+}
